3cf406df3d2de46b822ad79d69e691ab4b33f23f
zilch42 <tadaccount@me.com>
2024-09-29 08:27:57 +0930

    mac restart

--- plugin/restart/restart.py
+++ plugin/restart/restart.py
@@ -1,16 +1,23 @@
 import os
-from talon import Module, ui, actions, app
+import subprocess
+from talon import Module, ui, actions, Context
 
 mod = Module()
+ctx_win = Context()
+ctx_mac = Context()
+
+ctx_win.matches = r"""
+os: windows
+"""
+
+ctx_mac.matches = r"""
+os: mac
+"""
 
 @mod.action_class
 class Actions:
     def talon_restart():
         """restart talon"""
-        talon_app = ui.apps(pid=os.getpid())[0]
-        print("Restarting:", talon_app)
-        os.startfile(talon_app.exe)
-        talon_app.quit()
         
     def talon_quit():
         """quit talon"""
@@ -25,4 +32,30 @@ class Actions:
         def on_cancel():
             pass
         actions.user.are_you_sure_set_on_confirmation_action('Really restart talon?', on_confirm, on_cancel)
+
+    def quit_talon_with_confirmation():
+        '''Check whether to really quit talon'''
+        def on_confirm():
+            actions.user.talon_quit()
+        def on_cancel():
+            pass
+        actions.user.are_you_sure_set_on_confirmation_action('Really quit talon?', on_confirm, on_cancel)
         
+@ctx_win.action_class("user")
+class Actions:
+    def talon_restart():
+        """restart talon"""
+        talon_app = ui.apps(pid=os.getpid())[0]
+        print("Restarting:", talon_app)
+        os.startfile(talon_app.exe)
+        talon_app.quit()
+
+@ctx_mac.action_class("user")
+class Actions:
+    def talon_restart():
+        """restart talon"""
+        talon_app = ui.apps(pid=os.getpid())[0]
+        print("Restarting:", talon_app)
+        talon_path = talon_app.path
+        talon_app.quit()
+        subprocess.Popen(["open", talon_path])
--- plugin/restart/restart.talon
+++ plugin/restart/restart.talon
@@ -1,3 +1,2 @@
-os:windows
--
 restart talon: user.restart_talon_with_confirmation()
+quit talon: user.quit_talon_with_confirmation()
